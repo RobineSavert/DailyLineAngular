c0d6caec2f0fb044549afb681ec27fd556a26680
RobineSavert <robine@hotmail.nl>
2020-04-20 20:42:29 +0200

    Changed limit on dashboard & updated readme

--- src/app/diary/diary.component.ts
+++ src/app/diary/diary.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-diary',
   templateUrl: './diary.component.html',
   styleUrls: ['./diary.component.scss']
 })
-export class DiaryComponent {
+export class DiaryComponent implements OnInit {
+  constructor(private dataStorageService: DataStorageService) {
+  }
 
+  ngOnInit() {
+    this.dataStorageService.fetchLines().subscribe();
+  }
 }
